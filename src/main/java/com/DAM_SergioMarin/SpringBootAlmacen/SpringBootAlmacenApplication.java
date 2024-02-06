2090d311f245a811e55da87d9fbf099a11434921
Sergio <sergiomarincorrales@gmail.com>
2024-02-06 13:24:16 +0100

    Creación del proyecto

---
+++ src/main/java/com/DAM_SergioMarin/SpringBootAlmacen/SpringBootAlmacenApplication.java
@@ -0,0 +1,13 @@
+package com.DAM_SergioMarin.SpringBootAlmacen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAlmacenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAlmacenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/DAM_SergioMarin/SpringBootAlmacen/SpringBootAlmacenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.DAM_SergioMarin.SpringBootAlmacen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAlmacenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
